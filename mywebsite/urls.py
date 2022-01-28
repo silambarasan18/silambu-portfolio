871a6e2d8776e379d8b66988c9825c0837c99d54
silambarasan18 <silambu.tamil@outlook.com>
2022-01-28 22:25:17 +0530

    ninth added resume in pdf

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from base import views
 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
